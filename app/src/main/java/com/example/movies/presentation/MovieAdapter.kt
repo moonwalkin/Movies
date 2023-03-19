ad568de496a67380842840f0e5a6076137eb0685
Artem <art.samokysh@gmail.com>
2023-03-19 17:23:58 +0200

    add adapter

---
+++ app/src/main/java/com/example/movies/presentation/MovieAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.movies.presentation
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.example.movies.data.Movie
+import com.example.movies.databinding.LayoutMovieBinding
+
+class MovieAdapter : ListAdapter<Movie, MovieViewHolder>(MovieDifUtil) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        return MovieViewHolder(
+            LayoutMovieBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
---
+++ app/src/main/java/com/example/movies/presentation/MovieViewHolder.kt
@@ -0,0 +1,11 @@
+package com.example.movies.presentation
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.movies.data.Movie
+import com.example.movies.databinding.LayoutMovieBinding
+
+class MovieViewHolder(private val binding: LayoutMovieBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(movie: Movie) {
+
+    }
+}
