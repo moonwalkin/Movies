6831f0d68fdb75cd55346e8a6d47821082717af9
Artem <art.samokysh@gmail.com>
2023-03-20 11:47:07 +0200

    complete popular movies fragment

--- app/src/main/java/com/example/movies/data/CloudDataSourceImpl.kt
+++ app/src/main/java/com/example/movies/data/CloudDataSourceImpl.kt
@@ -2,8 +2,13 @@ package com.example.movies.data
 
 import javax.inject.Inject
 
-class CloudDataSourceImpl @Inject constructor(private val movieService: MovieService) : CloudDataSource {
+class CloudDataSourceImpl @Inject constructor(private val movieService: MovieService) :
+    CloudDataSource {
     override suspend fun fetchPopularMovies(): MoviesResponse {
-        return movieService.fetchPopularMovies()
+        return try {
+            movieService.fetchPopularMovies()
+        } catch (e: Exception) {
+            throw e
+        }
     }
 }
--- app/src/main/java/com/example/movies/presentation/MovieViewHolder.kt
+++ app/src/main/java/com/example/movies/presentation/MovieViewHolder.kt
@@ -1,11 +1,22 @@
 package com.example.movies.presentation
 
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.movies.data.Movie
 import com.example.movies.databinding.LayoutMovieBinding
 
-class MovieViewHolder(private val binding: LayoutMovieBinding) : RecyclerView.ViewHolder(binding.root) {
+class MovieViewHolder(private val binding: LayoutMovieBinding) :
+    RecyclerView.ViewHolder(binding.root) {
     fun bind(movie: Movie) {
-        binding.textView.text = movie.title
+        binding.apply {
+            tvTitle.text = movie.original_title
+            tvRating.text = movie.vote_average.toString()
+
+            Glide
+                .with(root)
+                .load("https://image.tmdb.org/t/p/w500${movie.poster_path}")
+                .into(ivPoster)
+        }
     }
+
 }
