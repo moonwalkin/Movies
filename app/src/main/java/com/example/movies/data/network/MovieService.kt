42d67ab586e7fa39e17e5a7f1f17d62927c875e9
Artem <art.samokysh@gmail.com>
2023-03-22 22:25:44 +0200

    final version

--- app/src/main/java/com/example/movies/data/network/MovieService.kt
+++ app/src/main/java/com/example/movies/data/network/MovieService.kt
@@ -8,15 +8,15 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MovieService {
-    @GET("movie/popular")
+    @GET("popular")
     suspend fun fetchPopularMovies(@Query("page") page: Int): MoviesResponse
 
-    @GET("movie/now_playing")
+    @GET("now_playing")
     suspend fun fetchLatestMovies(): MoviesResponse
 
-    @GET("movie/{movie_id}/videos")
+    @GET("{movie_id}/videos")
     suspend fun fetchTrailerById(@Path("movie_id") movieId: Int): TrailerResponse
 
-    @GET("movie/{movie_id}/credits")
+    @GET("{movie_id}/credits")
     suspend fun fetchActorsCast(@Path("movie_id") movieId: Int): CastResponse
 }
